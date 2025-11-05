c0388484f1aba59ef66aa06c754651cc7b372c5e
pOcOq38 <cylee0322@gmail.com>
2025-11-04 20:30:49 -0800

    commit

---
+++ week5/README.md
@@ -0,0 +1,9 @@
+## JavaScript Intro part 2
+
+Arrays are lists of items. You can instavtiate an array with the `[]` syntax.
+
+```js
+const myArray = [];
+```
+
+Arrays are index based, meaning that the first item is at index 0, the second at index1, and so on.
--- week5/practiceLoops.js
+++ week5/practiceLoops.js
@@ -1,3 +1,67 @@
 let a = [1, 2, 3, 4, 5];
 
-for (let i = 0; i < a.length; i++) {}
+for (let i = 0; i < a.length; i++) {
+  console.log(`The value is: ${i}`);
+}
+
+let fruits = ["apple", "banana", "cherry", "strawberry", "blueberry"];
+
+if (5 < 10) {
+  console.log("Yes, it is!");
+}
+
+if (5 > 10) {
+  console.log("Yes, it is!");
+} else {
+  console.log("No, it is not!");
+}
+
+//Exercise 1
+// Use loops, variables, and data stuructures however you would like to create a list of items:
+// a) Contained in both arrays
+//b) Contained in fruit plus the other list
+
+let fruit = ["apples", "grapes", "pineapples", "mangos"];
+let items = [
+  "black",
+  "gravy",
+  "t-shirts",
+  "pineapples",
+  "spaghetti",
+  "spinach",
+];
+let res = [];
+
+for (let i = 0; i < items.length; i++) {
+  for (let j = 0; j < fruit.length; j++) {
+    if (items[i] == fruit[j]) {
+      res = [items[i]];
+    }
+  }
+}
+console.log(`${res} is a fruit`);
+
+//Exercise: Solve it on your own
+function findMin(num1, num2) {
+  if (num1 < num2) {
+    return `The minimum number is: ${num1}`;
+  } else if (num1 > num2) {
+    return `The minimum number is: ${num2}`;
+  } else {
+    return "They are equal";
+  }
+}
+console.log(findMin(10, 20));
+
+//Ternary Operator
+let result = 3 > 5 ? "Yes" : "No";
+console.log(`result: ${result}`);
+
+function evenNum() {
+  let num = parseInt(document.getElementById("num").value);
+  if (num % 2 == 0) {
+    alert(`${num} is even number.`);
+  } else {
+    alert(`${num} is odd number.`);
+  }
+}
