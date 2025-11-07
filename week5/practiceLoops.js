4c7fc1920c86fcf0a7fd980f45da397e2395b80a
pOcOq38 <cylee0322@gmail.com>
2025-11-06 19:00:44 -0800

    Adds functions and week6 assignment

--- week5/practiceLoops.js
+++ week5/practiceLoops.js
@@ -67,3 +67,37 @@ function evenNum() {
     alert(`${num} is odd number.`);
   }
 }
+
+function cntEvenNum() {
+  let cnt = 0;
+  for (let i = 0; i < 4; i++) {
+    let num = parseInt(prompt("number:  "));
+    if (num % 2 == 0) {
+      cnt++;
+    }
+  }
+  document.getElementById("details4").innerHTML = `Even number counter: ${cnt}`;
+}
+
+function avgSumAbv() {
+  let sum = 0;
+  let cnt = 0;
+  let avg = sum / 5;
+
+  for (let i = 0; i < 5; i++) {
+    // get salaries
+    let sal = parseInt(prompt("salary:  "));
+
+    //sum
+    sum = sum + sal;
+
+    //count above 1000
+    if (sal > 1000) {
+      cnt++;
+    }
+  }
+
+  document.getElementById("sum").innerHTML = `sum: ${sum}`;
+  document.getElementById("average").innerHTML = `avg: ${avg}`;
+  document.getElementById("above1000").innerHTML = `cnt: ${cnt}`;
+}
