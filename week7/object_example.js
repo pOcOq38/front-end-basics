1e2393c98a2beefdaa5b1feb8fdcee0ac8881c0a
pOcOq38 <cylee0322@gmail.com>
2025-12-03 20:31:08 -0800

    Adds  week7

--- week7/object_example.js
+++ week7/object_example.js
@@ -1,14 +1,18 @@
- let computer = {
-    price: 1000,
-    color: "silver",
-    memory: "1TB",
- }
- 
- function Person(fname, lname, age){
-    this.fname= fname;
-    this.lname = lname;
-    this.age = age;
- }
- 
- var firstPerson = new Person("Michael", "Jordan", 49);
-  var secondPerson = new Person("Lara", "bili", 74);
+let computer = {
+  price: 1000,
+  color: "silver",
+  memory: "1TB",
+};
+console.log(computer);
+
+function Person(fname, lname, age) {
+  this.fname = fname;
+  this.lname = lname;
+  this.age = age;
+}
+
+var firstPerson = new Person("Michael", "Jordan", 49);
+var secondPerson = new Person("Lara", "bili", 74);
+
+console.log(firstPerson);
+console.log(secondPerson);
