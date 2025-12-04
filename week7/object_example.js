ed52aa300e91943aebe15b464aab9a4942a4d26b
pOcOq38 <cylee0322@gmail.com>
2025-12-03 18:32:29 -0800

    Adds week7

---
+++ week7/object_example.js
@@ -0,0 +1,14 @@
+ let computer = {
+    price: 1000,
+    color: "silver",
+    memory: "1TB",
+ }
+ 
+ function Person(fname, lname, age){
+    this.fname= fname;
+    this.lname = lname;
+    this.age = age;
+ }
+ 
+ var firstPerson = new Person("Michael", "Jordan", 49);
+  var secondPerson = new Person("Lara", "bili", 74);
